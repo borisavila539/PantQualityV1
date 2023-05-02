67c38f909df419e59825338f6bdaff001647c4a7
borisavila539 <68365374+borisavila539@users.noreply.github.com>
2023-05-02 14:57:23 -0600

    Nombre en Home Screen

--- src/components/Constant.tsx
+++ src/components/Constant.tsx
@@ -5,7 +5,6 @@ const TextoPantallas = parseInt(PixelRatio.getFontScale() * 15.22 + "");//16 - 2
 const ObjectHeigth = parseInt(PixelRatio.getFontScale() * 35.87 + "");//35 - 2
 const TextoHeader = parseInt(PixelRatio.getFontScale() * 17.39 + "");//18 - 2
 const IconHeader = parseInt(PixelRatio.getFontScale()*41.30 + "");//40 - 2
-const APILogin = "https://crm.intermoda.hn:41443/api/";
-const APIFinanzas = "http://localhost:5001/api/";
+const APIFinanzas = "http://10.100.1.11:8091/api/";
 const FontFamily = 'sans-serif';
-export { TextButtons, APILogin, TextoPantallas, FontFamily, ObjectHeigth, TextoHeader, APIFinanzas, IconHeader }
+export { TextButtons, TextoPantallas, FontFamily, ObjectHeigth, TextoHeader, APIFinanzas, IconHeader }
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,5 +1,5 @@
-import React , {FC}from 'react'
-import { Text, View, SafeAreaView, StyleSheet } from 'react-native';
+import React, { FC } from 'react'
+import { View, SafeAreaView, StyleSheet } from 'react-native';
 import Header from '../components/Header';
 import { grey } from '../components/colores';
 import { ScrollView } from 'react-native-gesture-handler';
@@ -9,45 +9,44 @@ import { RootStackParams } from '../navigation/Navigation';
 
 type props = StackScreenProps<RootStackParams, "LavadoScreen">;
 
-const HomeScreen:FC<props> = ({navigation}) => {
-  const onPressAuditoriaFinal =() =>{
+const HomeScreen: FC<props> = ({ navigation }) => {
+  const onPressAuditoriaFinal = () => {
     navigation.navigate("OrdenesScreen");
   }
 
-  const onPressOther = () =>{
+  const onPressOther = () => {
 
   }
   return (
     <View style={{ flex: 1, backgroundColor: grey }}>
-            <Header show={false} />
-            <ScrollView style={{ height: '100%', backgroundColor: grey }}>
-                <SafeAreaView style={styles.container}>
-                    <View style={styles.formulario}>
-                        <Buttons onPress={onPressAuditoriaFinal} disable={false} title='Auditoria Final' />
-                        <Buttons onPress={onPressOther} disable={false} title='Auditoria Partes' />
-                        <Buttons onPress={onPressOther} disable={false} title='Medias de J y Alturas de Bolsas' />
-                        <Buttons onPress={onPressOther} disable={false} title='Estandar de Bolsas' />
-        </View>
+      <Header show={false} />
+      <ScrollView style={{ height: '100%', backgroundColor: grey }}>
+        <SafeAreaView style={styles.container}>
+          <View style={styles.formulario}>
+            <Buttons onPress={onPressAuditoriaFinal} disable={false} title='Auditoria Final' />
+            <Buttons onPress={onPressOther} disable={false} title='Auditoria Partes' />
+            <Buttons onPress={onPressOther} disable={false} title='Medias de J y Alturas de Bolsas' />
+            <Buttons onPress={onPressOther} disable={false} title='Estandar de Bolsas' />
+          </View>
         </SafeAreaView>
-        </ScrollView>
-        <Text>Home</Text>
+      </ScrollView>
     </View>
   )
 }
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        width: '100%',
-        backgroundColor: grey,
-        alignItems: "center",
-        paddingVertical: 20
-    },
-    formulario: {
-        width: '80%',
-        maxWidth: 600,
-        justifyContent: "center",
-        alignItems: "center"
-    },
+  container: {
+    flex: 1,
+    width: '100%',
+    backgroundColor: grey,
+    alignItems: "center",
+    paddingVertical: 20
+  },
+  formulario: {
+    width: '80%',
+    maxWidth: 600,
+    justifyContent: "center",
+    alignItems: "center"
+  },
 });
 
 export default HomeScreen;
